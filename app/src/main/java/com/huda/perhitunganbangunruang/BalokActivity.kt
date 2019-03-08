6faf6b0a6019c34d7323c11f0eb791217f23ca14
Khoirul02 <hudak006@gmail.com>
2019-03-08 15:46:25 +0700

    perhitungan v-2

--- app/src/main/java/com/huda/perhitunganbangunruang/BalokActivity.kt
+++ app/src/main/java/com/huda/perhitunganbangunruang/BalokActivity.kt
@@ -11,18 +11,32 @@ class BalokActivity : AppCompatActivity() {
         setContentView(R.layout.activity_balok)
 
         btn_vbalok.setOnClickListener {
-            if (txtNumber1.text.toString().isEmpty() || txtNumber2.text.toString().isEmpty()) {
-                txtNumber1.setError("Semua input harus diisi!")
+            if (txtNumber1.text.toString().isEmpty() || txtNumber2.text.toString().isEmpty()
+                || textView3.text.toString().isEmpty()) {
+                txtNumber1.setError("Harus diisi bagian p*l*t!")
             } else {
                 val a = txtNumber1.text.toString().toDouble()
                 val b = txtNumber2.text.toString().toDouble()
                 val c = txtNumber3.text.toString().toDouble()
                 val d = this.hasil(a, b, c )
-                txtHasil.setText(d.toString())
+                txtHasil_balok.setText(d.toString())
+            }
+        }
+        btn_vbalok2.setOnClickListener {
+            if (txtNumber4.text.toString().isEmpty() || txtNumber5.text.toString().isEmpty()){
+                txtNumber4.setError("Harus diisi bagian la*t")
+            } else {
+                val a = txtNumber4.text.toString().toDouble()
+                val b = txtNumber5.text.toString().toDouble()
+                val c = this.hasil2(a,b)
+                txtHasil_balok.setText(c.toString())
             }
         }
     }
     fun hasil(a: Double, b: Double, c: Double): Double {
         return a * b * c
+    }
+    fun hasil2(a: Double, b: Double): Double{
+        return a * b
     }
 }
--- app/src/main/java/com/huda/perhitunganbangunruang/KubusActivity.kt
+++ app/src/main/java/com/huda/perhitunganbangunruang/KubusActivity.kt
@@ -2,11 +2,37 @@ package com.huda.perhitunganbangunruang
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_balok.*
+import kotlinx.android.synthetic.main.activity_kubus.*
 
 class KubusActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kubus)
+        btn_vkubus.setOnClickListener {
+            if (txtNumber6.text.toString().isEmpty()) {
+                txtNumber6.setError("Harus diisi bagian p*l*t!")
+            } else {
+                val a = txtNumber6.text.toString().toDouble()
+                val b = this.hasil(a)
+                txtHasil_kubus.setText(b.toString())
+            }
+        }
+        btn_vkubus2.setOnClickListener {
+            if (txtNumber7.text.toString().isEmpty()){
+                txtNumber7.setError("Harus diisi bagian la*t")
+            } else {
+                val a = txtNumber7.text.toString().toDouble()
+                val b= this.hasil2(a)
+                txtHasil_kubus.setText(b.toString())
+            }
+        }
+    }
+    fun hasil( a : Double):Double{
+        return a * a * a
+    }
+    fun hasil2(a : Double):Double{
+        return Math.sqrt(a) * Math.sqrt(a) * Math.sqrt(a)
     }
 }
